3e0e498c65397dad75a62b87decfd95109b2396b
nnton3 <akarnomazov@gmail.com>
2018-05-31 09:30:06 +0300

    update

---
+++ Assets/Strong_zombie/Start_Pack/Scripts/Camera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+
+	public GameObject target;
+	Vector3 targetVector;
+
+	void Start () {
+	}
+	
+	void Update () {
+		FindTarget (target);
+	}
+
+	void FindTarget(GameObject target) {
+		targetVector = target.transform.position;
+		targetVector.z -= 10f;
+		targetVector.y += 3f;
+		transform.position = targetVector;
+	}
+}
